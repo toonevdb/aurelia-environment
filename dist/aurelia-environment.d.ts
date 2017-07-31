2d3b2b9c37dff8fc3e77bd013594799cc15ed94c
Anthony Vanden Bossche <toonevdb@gmail.com>
2017-07-31 17:20:52 +0200

    test

--- dist/aurelia-environment.d.ts
+++ dist/aurelia-environment.d.ts
@@ -1,9 +1,18 @@
+declare global {
+    interface Window {
+        env: any
+    }
+
+    var env: any;
+}
+
+export declare function load(config?: {path?: string, file?: string}): Promise<any>;
 
 /**
  * An .env file content parser.
  */
 export declare class Parser {
-  
+
   /**
      * Parser function which can easily called to initialize the Parser and start
      * parsing the given string. It directly returns the resulting environment.
@@ -11,33 +20,33 @@ export declare class Parser {
      * @param content - The content string that will be parsed.
      */
   static parse(content: string): any;
-  
+
   /**
      * Environment object containing the parsed key-value-pairs of this Parser
      * object.
      */
   env: any;
-  
+
   /**
      * The number of lines parsed by this object.
      */
   lineNum: number;
   constructor();
-  
+
   /**
      * Parses the given content and returns the parsed environment object.
      *
      * @param content - content to parse
      */
   doParse(content: string): any;
-  
+
   /**
      * Splits the given string by line breaks and returns the array of lines.
      *
      * @param content - content to split by line endings
      */
   getLines(content: string): string[];
-  
+
   /**
      * Parses the given array of strings. If a line starts with a #, it will be
      * treated as a comment. Otherwise, the line will handed over to line parsing.
@@ -45,7 +54,7 @@ export declare class Parser {
      * @param lines - array of lines
      */
   parseContent(lines: string[]): any;
-  
+
   /**
      * Parses the given line and sets the resulting key-value pair as an
      * environment object property.
@@ -53,7 +62,7 @@ export declare class Parser {
      * @param line - the line reflecting a key-value pair
      */
   parseLine(line: string): void;
-  
+
   /**
      * Splits the given line by = and verifies that a key and a value exist.
      * Returns the key-value pair as an object.
@@ -62,14 +71,14 @@ export declare class Parser {
      * @throws ParserException
      */
   parseKeyValuePair(line: string): any;
-  
+
   /**
      * Strips comments from a value.
      *
      * @param value - the value to strip comments from
      */
   stripComments(value: string): string;
-  
+
   /**
      * Returns the environment object.
      */
